52eaeea4d6a69424db6aebb0596d290d6cfce39d
DESKTOP-765QD4C\Jan de Sterke <jandesterke@hotmail.com>
2021-04-08 21:41:05 +0200

    Cleaned up file tree.

--- assets/scripts/contenttimer.js
+++
@@ -1,9 +0,0 @@
-const contentBlock = document.querySelector(".c_contentblock__text");
-
-let totalWords = 0;
-
-totalWords += contentBlock.innerText.trim().split(" ").length;
-
-const estimatedReadingTime = totalWords / 4.16;
-
-visitData.estReadingTime = parseInt(estimatedReadingTime);
